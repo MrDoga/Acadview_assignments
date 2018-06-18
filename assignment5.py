55f9556161bb0cf5bb472487f4d217cb8088df4f
MrDoga <vasu.negi246@gmail.com>
2018-06-19 00:05:33 +0530

    decision control system (if,else,elif)

--- assignment5.py
+++ assignment5.py
@@ -1,7 +1,7 @@
 #Q1. Take input from user and check whether it is leap year or not
 
-year = input("enter your desired year: ")
-if(year%4==0):
+year = input("enter your desired year: ") #user will input year
+if(year%4==0): #leap year come every 4 year
     print("it is leap year")
 else:
     print("year is not leap year")
@@ -12,11 +12,11 @@ else:
 a = input("enter age of first person:")
 b = input("enter age of second person")
 c = input("enter age of third person")
-if (a > b & a > c):
+if (a > b & a > c): #if a is greater then b and c
     print("a is older then b and c")
-    if(b>c & b>a):
+    if(b>c & b>a):#if b is greater then a and c
         print("b is older then a and c ")
-        if(c>a & c>b):
+        if(c>a & c>b):#if c is greater then b and a
             print("c is older then a and b")
         else:
             print("c is younger then a and b")
